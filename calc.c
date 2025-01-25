c465e73030d3a4d5e87cd55022db50088291321a
barbozafernando <falandocomofernando@gmail.com>
2025-01-24 23:34:21 -0300

    change indentation and remove a few kinda useless vars

--- calc.c
+++ calc.c
@@ -1,43 +1,33 @@
 #include "calc.h"
 
 long long power_of_two(long long exp) {
-  long long result = 1;
+    long long r = 1;
 
-  for (; exp > 0; exp--){
-    result = result << 1;
-  }
+    for (; exp > 0; exp--)
+      r = r << 1;
 
-  return result;
+    return r;
 }
 
 long long convert(char* binary) {
-  size_t binary_len = strlen(binary);
-  long long temp = binary_len - 1;
-  long long final_array[binary_len];
-  long long decimal = 0;
+    size_t binary_len = strlen(binary);
+    long long temp = binary_len - 1;
+    long long result = 0;
 
-  for(size_t i = 0; i < binary_len; i++) {
-    long long acc, result = 0;
-    long long *p_result = &result;
-    long long bit = binary[i] - 48;
+    for(size_t i = 0; i < binary_len; i++) {
+      long long c = 1;
+      long long bit = binary[i] - 48;
 
-    *p_result = bit * power_of_two(temp);
+      c = bit * power_of_two(temp);
 
-    if (*p_result == 0) {
-      *p_result = power_of_two(temp);
+      if (c == 0)
+        c = power_of_two(temp);
+
+      result += c * bit;
+
+      if (temp != 0)
+        temp--;
     }
 
-    acc = *p_result * bit;
-    final_array[i] = acc;
-
-    if (temp != 0) {
-      temp--;
-    }
-  }
-
-  for(size_t i = 0; i < binary_len; i++) {
-    decimal += final_array[i];
-  }
-
-  return decimal;
+    return result;
 }
