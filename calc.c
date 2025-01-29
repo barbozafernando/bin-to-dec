4b67a4e180a20e9eb78ee6cdc2909d04f26f6916
barbozafernando <falandocomofernando@gmail.com>
2025-01-29 18:49:49 -0300

    we support 64 bits signed binaries now

--- calc.c
+++ calc.c
@@ -1,32 +1,18 @@
+#include <string.h>
+
 #include "calc.h"
 
-long long power_of_two(long long exp) {
-    long long r = 1;
-
-    for (; exp > 0; exp--)
-        r = r << 1;
-
-    return r;
-}
-
-long long convert(char* binary) {
+long long b2d_convert(char* binary, size_t treat_as_signed) {
     size_t binary_len = strlen(binary);
-    long long temp = binary_len - 1;
-    long long result = 0;
+    long long MASK    = 1LL << (binary_len - 1);
+    long long result  = 0;
 
     for(size_t i = 0; i < binary_len; i++) {
-        long long c = 1;
-        long long bit = binary[i] - 48;
+        result = result * 2 + (binary[i] - 48);
+    }
 
-        c = bit * power_of_two(temp);
-
-        if (c == 0)
-            c = power_of_two(temp);
-
-        result += c * bit;
-
-        if (temp != 0)
-            temp--;
+    if (treat_as_signed == 1 && (result & MASK)) {
+        return result * -1;
     }
 
     return result;
